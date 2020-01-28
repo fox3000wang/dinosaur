d2c603c3234c8a92975140469f7dc310ceff4182
fox.wang <fox3000wang@hotmail.com>
2020-01-28 10:10:53 +0800

    实现远程加载dicom文件

--- src/container/DiconPage/index.tsx
+++ src/container/DiconPage/index.tsx
@@ -8,9 +8,6 @@ import dicomParser from "dicom-parser";
 import { IAppActionProps, mapAppActions } from "../../action";
 import { Background, Layout, Head, Body, Left, Center, Icon } from "./style";
 
-// https://styled-icons.js.org/
-//import { Lock } from "styled-icons/material";
-
 function mapStateToProps() {
   return {};
 }
@@ -24,50 +21,69 @@ class DiconPage extends React.Component<IAppActionProps, any> {
     cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
   }
 
-  // componentDidMount() {
-  //   cornerstoneWADOImageLoader.wadouri.loadFileRequest(
-  //     // "http://106.13.103.146/demo.dicom"
-  //     "file:///Users/fox/Downloads/demo.dcm"
-  //   );
-  // }
+  componentDidMount() {
+    const url = `https://efilm-cro.obs.cn-north-1.myhuaweicloud.com/410028/upload/b07ce1068dc649f2969489efe0bc476f/1.2.840.113619.2.203.4.2147483647.1503418397.288740/1.3.12.2.1107.5.8.2.100063.201802281756062921761.4.dcm`;
+    this.loadAndViewImage(url);
+  }
 
-  fileChange = e => {
-    const file = e.target.files[0];
-    const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
-
-    console.log(file);
-    console.log(imageId);
-    this.loadAndViewImage(imageId);
-  };
-
-  loadAndViewImage = imageId => {
-    const element = document.getElementById("dicomImage");
-
+  loadAndViewImage = url => {
+    var element = document.getElementById("dicomImage");
     cornerstone.enable(element);
 
-    const start = new Date().getTime();
-
-    cornerstone.loadImage(imageId).then(
-      image => {
-        console.log(image);
-        const viewport = cornerstone.getDefaultViewportForImage(element, image);
-
-        cornerstone.displayImage(element, image, viewport);
-        if (this.loaded === false) {
-          cornerstoneTools.mouseInput.enable(element);
-          cornerstoneTools.mouseWheelInput.enable(element);
-          cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
-          cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
-          cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-          cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
-          cornerstoneTools.imageStats.enable(element);
-          this.loaded = true;
+    cornerstoneWADOImageLoader.wadouri.dataSetCacheManager
+      .load(url, cornerstoneWADOImageLoader.internal.xhrRequest)
+      .then(dataSet => {
+        // dataset is now loaded, get the # of frames so we can build the array of imageIds
+        var numFrames = dataSet.intString("x00280008");
+        if (!numFrames) {
+          alert("Missing element NumberOfFrames (0028,0008)");
+          return;
         }
-      },
-      err => {
-        alert(err);
-      }
-    );
+
+        const imageIds = [];
+        const imageIdRoot = "wadouri:" + url;
+
+        for (let i = 0; i < numFrames; i++) {
+          const imageId = imageIdRoot + "?frame=" + i;
+          imageIds.push(imageId);
+        }
+
+        const stack = {
+          currentImageIdIndex: 0,
+          imageIds: imageIds
+        };
+
+        cornerstone.loadAndCacheImage(imageIds[0]).then(
+          image => {
+            console.log(image);
+            // now that we have an image frame in the cornerstone cache, we can decrement
+            // the reference count added by load() above when we loaded the metadata.  This way
+            // cornerstone will free all memory once all imageId's are removed from the cache
+            cornerstoneWADOImageLoader.wadouri.dataSetCacheManager.unload(url);
+
+            cornerstone.displayImage(element, image);
+            if (this.loaded === false) {
+              // ???
+              // cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+
+              // Set the stack as tool state
+              cornerstoneTools.addStackStateManager(element, [
+                "stack",
+                "playClip"
+              ]);
+              cornerstoneTools.addToolState(element, "stack", stack);
+              // Start playing the clip
+              // TODO: extract the frame rate from the dataset
+              var frameRate = 10;
+              cornerstoneTools.playClip(element, frameRate);
+              this.loaded = true;
+            }
+          },
+          err => {
+            alert(err);
+          }
+        );
+      });
   };
 
   render() {
@@ -76,17 +92,7 @@ class DiconPage extends React.Component<IAppActionProps, any> {
         <Layout>
           <Head>{/* <Lock size="24"></Lock> */}</Head>
           <Body>
-            <Left>
-              <div>
-                <form id="form">
-                  <input
-                    type="file"
-                    id="selectFile"
-                    onChange={this.fileChange}
-                  />
-                </form>
-              </div>
-            </Left>
+            <Left></Left>
             <Center id="dicomImage"></Center>
           </Body>
         </Layout>
@@ -96,3 +102,46 @@ class DiconPage extends React.Component<IAppActionProps, any> {
 }
 
 export default connect(mapStateToProps, mapAppActions)(DiconPage);
+
+/*
+  <div>
+    <form id="form">
+      <input
+        type="file"
+        id="selectFile"
+        onChange={this.fileChange}
+      />
+    </form>
+  </div>
+
+  fileChange = e => {
+    const file = e.target.files[0];
+    const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+    this.loadAndViewImage(imageId);
+  };
+  
+  loadAndViewImage = imageId => {
+    const element = document.getElementById("dicomImage");
+    cornerstone.enable(element);
+    cornerstone.loadImage(imageId).then(
+      image => {
+        console.log(image);
+        const viewport = cornerstone.getDefaultViewportForImage(element, image);
+        cornerstone.displayImage(element, image, viewport);
+        if (this.loaded === false) {
+          cornerstoneTools.mouseInput.enable(element);
+          cornerstoneTools.mouseWheelInput.enable(element);
+          cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+          cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+          cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+          cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+          // cornerstoneTools.imageStats.enable(element);
+          this.loaded = true;
+        }
+      },
+      err => {
+        alert(err);
+      }
+    );
+  };
+  */
